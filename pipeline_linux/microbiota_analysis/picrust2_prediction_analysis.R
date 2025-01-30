8e7f95e6a8b5d1e1578dab79ab5e42ac963b99e8
bioth <th.maumy@gmail.com>
2025-01-29 20:44:37 -0500

    addition of function to generate visuals based on ko annotations for compounds

--- pipeline_linux/microbiota_analysis/picrust2_prediction_analysis.R
+++ pipeline_linux/microbiota_analysis/picrust2_prediction_analysis.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(ggprism)
 library(patchwork)
 
+
+source("~/Documents/CHUM_git/gut-microbiota-iron/pipeline_linux/microbiota_analysis/utilities.R")
 # Installing ggpicrust2 and other required libraries
 {
 # Had to install GSL via "sudo apt-get install libgsl-dev" from command line, for Aldex2
@@ -28,7 +30,7 @@ devtools::install_github("cafferychen777/ggpicrust2")
 setwd("~/Documents/CHUM_git/Microbiota_17/picrust2/input/picrust2_out_pipeline/")
 
 pathways <- read.table("pathways_out/path_abun_unstrat.tsv.gz", sep = "\t", header = TRUE)
-ko_metagenome <- list(read.table("KO_metagenome_out/pred_metagenome_unstrat.tsv.gz", sep = "\t", header = TRUE),
+ko_metagenome <- list(read.table("KO_metagenome_out/pred_metagenome_unstrat.tsv.gz", sep = "\t", header = TRUE, row.names = 1),
                       read.table("KO_metagenome_out/seqtab_norm.tsv.gz", sep = "\t", header = TRUE),
                       read.table("KO_metagenome_out/weighted_nsti.tsv.gz", sep = "\t", header = TRUE))
                     
@@ -37,7 +39,7 @@ View(ko_metagenome[[2]]) # osef
 View(ko_metagenome[[3]]) # osef
 
 
-ec_metagenome <- list(read.table("EC_metagenome_out/pred_metagenome_unstrat.tsv.gz", sep = "\t", header = TRUE),
+ec_metagenome <- list(read.table("EC_metagenome_out/pred_metagenome_unstrat.tsv.gz", sep = "\t", header = TRUE, row.names = 1),
                       read.table("EC_metagenome_out/seqtab_norm.tsv.gz", sep = "\t", header = TRUE),
                       read.table("EC_metagenome_out/weighted_nsti.tsv.gz", sep = "\t", header = TRUE))
 
@@ -53,7 +55,7 @@ ko_predicted <- read.table("KO_predicted.tsv.gz", sep = "\t", header = TRUE, row
 
 # Remove the X at the start of the names
 colnames(ko_metagenome[[1]]) = substring(colnames(ko_metagenome[[1]]), first = 2, 6)
-colnames(ko_metagenome[[1]])[1] = "KO_id"
+# colnames(ko_metagenome[[1]])[1] = "KO_id"
 
 # Metadata handling
 {
@@ -111,81 +113,51 @@ data("metadata")
 data("ko_abundance")
 } # This is pretty much awful and does not make any sense at all, I guess we are doing it on our own
 
+# Load file with KOs annotations for compound/pathways of interest
+ko_annotations <- readxl::read_excel("~/Documents/CHUM_git/picrust2 database/compound_to_kos.xlsx")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Select SCFA-related KOs
-scfa_kos <- c("K00929", "K01034", "K00169")  # Example KOs for butyrate and propionate
-
-# Extract SCFA KO abundances
-scfa_abundance <- ko_metagenome[[1]][rownames(ko_metagenome[[1]]) %in% scfa_kos, ]
-
-# Sum SCFA KO abundances for each sample
-scfa_total <- as.data.frame(colSums(scfa_abundance))
-
-# data <- scfa_abundance[1,]  %>% 
-#   pivot_longer(
-#     cols = 1:ncol(scfa_abundance), 
-#     names_to = "sample",
-#     values_to = "count"
-#   )
-
-# Metadata handling
-{
-metadata <- read.csv("~/Documents/CHUM_git/Microbiota_17/metadata/metadata.csv", sep = ";")
-
-#adding first col as rownames too
-rownames(metadata) <- metadata$sample_id
-
-#transforming week col from num to character
-metadata$week <- as.factor(metadata$week)
-metadata$diet <- as.factor(metadata$diet)
-
-# Replaces Samuel names by shorter versions
-metadata$treatment <- gsub(".*Putrescine.*", "Putrescine", metadata$treatment)
-metadata$treatment <- gsub(".*Vehicle.*", "Vehicle", metadata$treatment)
-metadata$genotype <- gsub(".*IL-22.*", "IL-22ra1-/-", metadata$genotype)
-
-# Creates gg_group specific to Claire
-metadata$gg_group[metadata$student == "Claire"] <- 
-  paste(metadata$week[metadata$student == "Claire"], 
-        metadata$diet[metadata$student == "Claire"], 
-        sep = ":")
-
-# Creates gg_group specific to Samuel
-metadata$gg_group[metadata$student == "Samuel"] <- 
-  paste(metadata$genotype[metadata$student == "Samuel"], 
-        metadata$treatment[metadata$student == "Samuel"], 
-        sep = ":")
+# Function that iterates through list of compounds/pathways, subtract KO_abundance dataframe for KOs associated, and returns multiple graphs
+# ko_abundance needs to have kos as rownames
+KOsToCompoundAbundanceGraphs <- function(ko_abundance, ko_annotations, metadata, group, group_order, sample_id_col, path){
+  
+  existingDirCheck(path)
+  
+  # Iterate through the compounds/pathways
+  for(i in 1:nrow(ko_annotations)){
+    compound <- ko_annotations$`compound/pathway`[i]
+    kos <- unlist(strsplit(ko_annotations$Kos[i], ";"))
+    abundance <- ko_abundance[rownames(ko_abundance) %in% kos, ]
+    total_ab <- as.data.frame(colSums(abundance))
+    
+    # Associate gg_group with each sample_id using metadata information
+    for(i in 1:nrow(total_ab)){
+    total_ab[[group]][i] <- metadata[metadata[[sample_id_col]]==rownames(total_ab)[i],group]
+    }
+    
+    # Replacing a colname and adding a sample_id column, + gg_plot as a factor
+    colnames(total_ab)[1] <- compound
+    total_ab$sample_id <- rownames(total_ab)
+    total_ab[[group]] <- factor(total_ab[[group]], levels = group_order)
+    
+    p <- ggplot(total_ab, aes(x = .data[[group]], y = .data[[compound]], colors = .data[[group]])) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      labs(title = paste("Predicted", clean_string(compound), "levels", sep = " "), y = paste("Normalized", clean_string(compound), sep = " "))
+    p
+    
+    # Save plot
+    ggsave(plot = p, filename = paste0(path, "/", clean_string(compound), "_predicted.png"), dpi = 300, width = 5, height = 5, bg = "white")
+  }
 }
 
-# Associate gg_group with each sample_id using metadata information
-for(i in 1:nrow(scfa_total)){
-  scfa_total$gg_group[i] <- metadata[metadata[rownames(metadata)==rownames(scfa_total)[i],"sample_id"],"gg_group"]
-}
+KOsToCompoundAbundanceGraphs(ko_abundance = ko_metagenome[[1]],
+                            ko_annotations = ko_annotations,
+                            metadata = meta,
+                            group = "gg_group",
+                            group_order = c("Wt:Vehicle", "Wt:Putrescine", "IL-22ra1-/-:Vehicle", "IL-22ra1-/-:Putrescine"),
+                            sample_id_col="sample_id",
+                            path = "~/Documents/CHUM_git/figures/samuel/picrust2")
 
-# Replacing a colname and adding a sample_id column, + gg_plot as a factor
-colnames(scfa_total)[1] <- "scfa_ab"
-scfa_total$sample_id <- rownames(scfa_total)
-factor(scfa_total$gg_group, levels = c("Wt:Vehicle", "Wt:Putrescine", "IL-22ra1-/-:Vehicle", "IL-22ra1-/-:Putrescine"))
 
-# Visualize with ggplot2
-# df <- data.frame(Sample = names(scfa_total), SCFA_Potential = scfa_total)
 
-p <- ggplot(scfa_total, aes(x = gg_group, y = scfa_ab, colors = gg_group)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() +
-  labs(title = "Predicted butyrate + propionate production", y = "Butyrate + propionate")
-p
+
